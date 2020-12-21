670f971b07b4995941ac35647a7408917da005a8
Robson <robmartinhao@gmail.com>
2020-12-21 20:37:52 -0300

    Loop e Condicional

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -2,5 +2,15 @@ object HelloWorld {
 
     def main(args: Array[String]): Unit = {
         println("Hello World " + args{0} + "!")
+
+        var i = 0
+        while (i < args.length) {
+            
+            if(i % 2 == 0)
+                println(args(i))
+            else
+                println("X")
+            i += 1
+        }
     }
 }
