e565753d2ac2728737ce4c25ff7d0de62dd74a35
Robson <robmartinhao@gmail.com>
2021-01-05 21:02:08 -0300

    Recursividade

---
+++ HelloDSA/src/main/scala/capitulo3/e04.scala
@@ -0,0 +1,22 @@
+package capitulo3
+
+object e04 {
+  def gcdLoop(x: Long, y: Long): Long = {
+    var a = x
+    var b = y
+    while (a != 0) {
+      val temp = a
+      a = b % a
+      b = temp
+    }
+    b
+  }
+
+  def gcd(x: Long, y: Long): Long =
+    if (y ==0) x else gcd(y, x % 2)
+
+  def main(args: Array[String]): Unit = {
+    println(gcdLoop(15, 10))
+    println(gcd(10, 15))
+  }
+}
