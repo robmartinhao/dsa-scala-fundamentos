184ec463d51ca1a563c8eeedd98e7cb62f9f206a
Robson <robmartinhao@gmail.com>
2021-01-05 21:33:51 -0300

    Loop For - Parte 2

--- HelloDSA/src/main/scala/capitulo3/e05.scala
+++ HelloDSA/src/main/scala/capitulo3/e05.scala
@@ -44,12 +44,24 @@ object e05 {
 //      println(file)
 //    }
 
-    val filesHere = new File("src/main/scala/capitulo3").listFiles
-    for(
-      file <- filesHere
-      if file.isFile
-      if file.getName.endsWith(".scala")
-    )
-    println(file)
+//    val filesHere = new File("src/main/scala/capitulo3").listFiles
+//    for(
+//      file <- filesHere
+//      if file.isFile
+//      if file.getName.endsWith(".scala")
+//    )
+//    println(file)
+
+//    for (i <- 1 to 3; j <- 1 to 3)
+//      println((10 * i + j) + " ")
+
+//    for (i <- 1 to 3; j <- 1 to 3 if i != j)
+//      println((10 * i + j) + " ")
+
+//    val collection = for (i <- 1 to 10) yield i % 3
+//      println(collection)
+
+    val myString = for (c <- "Hello"; i <- 0 to 1) yield (c + i).toChar
+      println(myString)
   }
 }
