c4f22510d0b9fa5eda33047c199b4f1773f0ec19
Robson <robmartinhao@gmail.com>
2021-01-07 18:34:31 -0300

    Expressões Match

---
+++ HelloDSA/src/main/scala/capitulo3/e10.scala
@@ -0,0 +1,27 @@
+package capitulo3
+
+object e10 {
+
+  def main(args: Array[String]): Unit = {
+
+    //    val firstArg = if (args.length > 0) args(0) else ""
+    //
+    //    firstArg match {
+    //      case "S" => println("Scala")
+    //      case "J" => println("Java")
+    //      case "P" => println("Python")
+    //      case _ => println("E agora?")
+    //    }
+
+    val firstArg = if (!args.isEmpty) args(0) else ""
+
+    val friend =
+      firstArg match {
+        case "S" => "Scala"
+        case "J" => "Java"
+        case "P" => "Python"
+        case _ => "E agora?"
+      }
+    println(friend)
+  }
+}
